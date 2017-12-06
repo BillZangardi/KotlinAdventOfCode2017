0a6b79359336b35edecc77e3f01b10b1934a4961
Bill Zangardi <bill.zangardi@gmail.com>
2017-12-05 22:14:52 -0500

    Somewhat print the matrix for day 3....

--- src/day3/Day3.kt
+++ src/day3/Day3.kt
@@ -7,27 +7,41 @@ fun main(args: Array<String>) {
 }
 
 fun partOne(): String {
-    val input = 9
+    val input = 25
     val dimensions = ceil(sqrt(input.toDouble()))
     val center = floor(sqrt(dimensions)).toInt()
     var currentX = center
     var currentY = center
-    var graph = Array(dimensions.toInt()) { IntArray(dimensions.toInt()) }
-    for (i in 1..input) {
-//        val right = graph[currentX + 1][currentY]
-//        var up = graph[currentX][currentY - 1]
-//        var left = graph[currentX - 1][currentY]
-//        var down = graph[currentX][currentY + 1]
-        if(graph[currentX][currentY] == 0) {
-            graph[currentX][currentY] = i
-        }
-        println("${sqrt(i.toDouble())}")
+    var graph = Array(dimensions.toInt()) {
+        IntArray(dimensions.toInt())
     }
-//    graph[center][center] = 1
-//    graph.forEach{ value ->
-//        value.forEach{ value2 ->
-//            println(value2)
-//        }
-//    }
+    var currentSquareDiameter = 1
+    for (i in 1..input) {
+        if(currentX >=0 && currentY >= 0) {
+            graph[currentY][currentX] = i
+            if (i % 2 != 0 && i * i == currentSquareDiameter) {
+                currentX++
+                currentSquareDiameter++
+            } else if (i == currentSquareDiameter) {
+                currentY--
+            } else if (i == currentSquareDiameter * currentSquareDiameter + 1) {
+                currentY++
+                currentSquareDiameter++
+            } else {
+                currentX--
+            }
+        }
+    }
+    graph.forEach { value ->
+        value.forEach { value2 ->
+            print("$value2\t")
+        }
+        print("\n")
+    }
     return "$dimensions x $dimensions"
+}
+
+fun isPrime(num: Int): Boolean {
+    val flag = (2..num / 2).any { num % it == 0 }
+    return !flag
 }
