3b2f542805144880d25a612c24170599b46daf75
Bill Zangardi <bill.zangardi@gmail.com>
2017-12-05 21:27:39 -0500

    Day 5 part 2

--- src/day5/Day5.kt
+++ src/day5/Day5.kt
@@ -5,18 +5,24 @@ import java.io.InputStream
 
 fun main(args: Array<String>) {
     val inputStream: InputStream = File("/Users/zangardiw/Sandbox/KotlinAdventOfCode2017/src/day5/Day5Input.txt").inputStream()
-    val lineList = mutableListOf<Int>()
-    inputStream.bufferedReader().useLines { lines -> lines.forEach { lineList.add(it.toInt()) } }
-    println("Part one ${partOne(lineList)}")
-    println("Part two ${partTwo(lineList)}")
+    val lineList1 = mutableListOf<Int>()
+    val lineList2 = mutableListOf<Int>()
+    inputStream.bufferedReader().useLines { lines ->
+        lines.forEach {
+            lineList1.add(it.toInt())
+            lineList2.add(it.toInt())
+        }
+    }
+    println("Part one ${partOne(lineList1)}")
+    println("Part two ${partTwo(lineList2)}")
 }
 
 fun partOne(input: MutableList<Int>): Int {
     var total = 0
     var currentIndex = 0
-    while (currentIndex < input.size) {
-        var jump = input[currentIndex]
-        input[currentIndex] = input[currentIndex] + 1
+    while (currentIndex >= 0 && currentIndex < input.size) {
+        val jump = input[currentIndex]
+        input[currentIndex] = input[currentIndex].inc()
         currentIndex += jump
         total++
     }
@@ -25,5 +31,16 @@ fun partOne(input: MutableList<Int>): Int {
 
 fun partTwo(input: MutableList<Int>): Int {
     var total = 0
+    var currentIndex = 0
+    while (currentIndex >= 0 && currentIndex < input.size) {
+        val jump = input[currentIndex]
+        if (jump >= 3) {
+            input[currentIndex] = input[currentIndex].dec()
+        } else {
+            input[currentIndex] = input[currentIndex].inc()
+        }
+        currentIndex += jump
+        total++
+    }
     return total
 }
